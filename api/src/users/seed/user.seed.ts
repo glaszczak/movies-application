b0e003188a8d951872585626d9c90dfacd885524
Grzegorz <grzegorz.laszczak@gmail.com>
2021-07-15 23:02:46 +0200

    Fix user seed data

--- api/src/users/seed/user.seed.ts
+++ api/src/users/seed/user.seed.ts
@@ -8,7 +8,7 @@ export const usersSeed = [
     password: 'sR-_pcoow-27-6PAwCD8',
   },
   {
-    userId: 456,
+    userId: 434,
     username: 'premium-jim',
     role: UserRole.PREMIUM,
     password: 'GBLtTyq3E_UNjFnpo9m6',
