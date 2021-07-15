1596635639da2d4702c9acad3a7683be16617cca
Grzegorz <grzegorz.laszczak@gmail.com>
2021-07-15 23:05:13 +0200

    Fix expires in for jwt strategy

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get('SECRET_JWT_KEY'),
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '1800s' },
       }),
     }),
   ],
